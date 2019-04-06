2ee7e7f1226debd94317fff61ba3b62c8cbaa156
Lliuhaoxin <1917089423@qq.com>
2019-04-06 12:50:04 +0800

    A+B

---
+++ L1001 A+B Format.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    string re = to_string(a + b);
+    for (int i = 0; i < re.length(); i++)
+    {
+        cout << re[i];
+        if (re[i] == '-')
+            continue;
+        if ((re.length() - i - 1) % 3 == 0 && i != re.length() - 1)
+            cout << ",";
+    }
+    cout << endl;
+}
